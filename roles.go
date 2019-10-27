8b2e45e75a05a297ce02fe0c2a56b38a52ab4164
Michael Koeppl <calmandniceperson@protonmail.com>
2019-10-27 07:09:52 +0100

    Add 'Pattern' attribute to Role struct

--- roles.go
+++ roles.go
@@ -8,6 +8,7 @@ type RolesWithUsers map[string][]string
 // Role represents a role as it is implemented in the Role Strategy plugin.
 // https://github.com/runzexia/role-strategy-plugin/blob/master/src/main/java/com/michelin/cio/hudson/plugins/rolestrategy/Role.java
 type Role struct {
-	PermissionIDs map[string]bool
-	SIDs          []string
+	PermissionIDs map[string]bool `json:"permissionIds"`
+	Pattern       string          `json:"pattern"`
+	SIDs          []string        `json:"sids"`
 }
